193e41e3fdb7ac2e2c8c50afda3018c2ae7f20a9
bishalagrahari <46011774+bishalagrahari@users.noreply.github.com>
2018-12-20 07:22:39 +0545

    Create area.c

---
+++ area.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float l, b, A;
+clrscr(f);
+printf("\n enter length");
+scanf("%f", &l);
+printf("\n enter breadth");
+scanf("%f", &b);
+A=l*b;
+printf("\n perimeter=%f",A);
+getch();
+}
